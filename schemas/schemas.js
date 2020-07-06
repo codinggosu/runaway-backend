a7c7c792c0ee048dcac01e55ea938cfe3e224bab
cedricblaise <cblaise@bowdoin.edu>
2020-07-06 04:14:33 -0400

    Event route and api documentation

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -1,31 +1,131 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-module.exports = {
-    chatSchema: {
-        messages: [ 
-            { 
-                sender: { type: String, required: true },
-                message: { type: String, required: true } 
-            } 
-        ] 
-    },
+/**
+ * @swagger
+ * tags:
+ *     - name: Chat
+ *     - name: Blog
+ *     - name: Event
+ *  
+ * components:
+ *      schemas:
+ *          Chat:
+ *              type: object
+ *              properties:
+ *                  messages:
+ *                      type: array
+ *                      items: 
+ *                          type: object
+ *                          properties: 
+ *                              sender: 
+ *                                  type: string
+ *                              message:
+ *                                  type: string
+ *                          required: 
+ *                              - sender
+ *                              - message
+ *              required:
+ *                  - messages 
+ * 
+ *          Blog:
+ *              type: object
+ *              properties:
+ *                  user:
+ *                      type: string
+ *                  title: 
+ *                      type: string
+ *                  date: 
+ *                      type: string
+ *                  content: 
+ *                      type: string 
+ *                  likes: 
+ *                      type: number
+ *                  url: 
+ *                      type: string
+ *                  comments:
+ *                      type: array
+ *                      items:
+ *                          type: string
+ *              required: [user, title, date, content, url]
+ * 
+ *          Event:
+ *              type: object
+ *              properties: 
+ *                  organizer: 
+ *                      type: string
+ *                  title:
+ *                      type: string
+ *                  date: 
+ *                      type: string
+ *                  content: 
+ *                      type: string
+ *                  location: 
+ *                      type: object
+ *                      properties:
+ *                          type: 
+ *                              type: string
+ *                              enum: [Point]
+ *                          coordinates:
+ *                              type: array
+ *                              items: 
+ *                                  type: number
+ *                              minItems: 2
+ *                              maxItems: 2
+ *                      required: 
+ *                          - type
+ *                          - coordinates
+ * 
+ *              required: [organizer, title, date, content, location]        
+ */
 
-    userSchema: {
+module.exports = {
+
+    chatSchema: new Schema({
+        messages: { 
+            type: [ 
+                { 
+                    sender: { type: String, required: true },
+                    message: { type: String, required: true } 
+                } 
+            ],
+            required: true 
+        }
+    }),
+
+    userSchema: new Schema({
         name: { type: String, required: true },
         email: { type: String, required: true },
         password: { type: String, required: true }
-    },
+    }),
 
     contentSchema: {},
 
-    blogSchema: {
-        author: { type: String},
+    blogSchema: new Schema({
+        user: { type: Schema.Types.ObjectId, ref: "User", required: false },
         title: { type: String, required: true }, 
-        date: { type: String, default: Date.now }, 
+        date: { type: String, default: new Date().toJSON() }, 
         content: { type: String, required: true },
         likes: { type: Number, required: false, default: 0 },
         url: { type: String, required: true, unique: true },
         comments: [ { type: String, required: false } ]
-    },
+    }),
+
+    eventSchema: new Schema({
+        organizer: { type: String, required: true },
+        title: { type: String, required: true },
+        date: { type: String, default: new Date().toJSON() },
+        content: { type: String, required: true },
+        location: { 
+            type: {
+                type: String,
+                enum: ["Point"],
+                required: true,
+            },
+            coordinates: {
+                type: [Number],
+                required: true
+            }
+        }
+    })
 }
--- server.js
+++ server.js
@@ -9,10 +9,28 @@ const app = express();
 const PORT = process.env.PORT || 7000;
 const server = http.createServer(app);
 
+// Swagger documentation
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express"); 
+
+const options = {
+    definition: {
+        openapi: "3.0.0", // Specification (optional, defaults to swagger: '2.0')
+        info: {
+            title: "Runaway API", // Title (required)
+            version: "1.0.0", // Version (required)
+            servers: ["http://localhost:7000"]
+        },
+    },
+        // Path to the API docs
+        apis: ["./route/*.js", "./schemas/schemas.js"]
+    };
+
 //Routes
 const blogRoute = require("./route/blog.js");
 const chatRoute = require("./route/chat/chat.js");
 const chatDataHandler = require("./route/chat.js"); //to be merged into chatRoute soon
+const eventRoute = require("./route/event.js");
 
 //connect to the database // for now, the password will be in the file
 mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://runaway-database:2gn5YAq0BRwLc7tF@runaway-mrvci.mongodb.net/runaway?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
@@ -23,11 +41,14 @@ db.once("open", () => {
     console.log("Connection succeeded");
 });
 
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc(options);
+
 //MiddleWare
 app.use(Cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 
 
@@ -41,7 +62,7 @@ blogRoute(app,mongoose);
 auth(app,mongoose);
 chatRoute(app,mongoose,server);
 chatDataHandler(app,mongoose);
-
+eventRoute(app, mongoose);
 
 app.set("json spaces", 2);
 
