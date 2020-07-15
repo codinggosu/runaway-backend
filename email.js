b1f61662ce4c5f68e6996158891317d84839f706
codinggosu <leedongjoo96@naver.com>
2020-07-15 20:33:44 +0900

    finished email sending logic

--- email.js
+++ email.js
@@ -1,38 +1,27 @@
-"use strict";
-const nodemailer = require("nodemailer");
+require('dotenv').config()
 
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
+const nodemailer = require('nodemailer');
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'runawayengineering@gmail.com',
+    pass: process.env.EMAIL_PASSWORD
+  }
+});
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"practicum 👻" <practicum@example.com>', // sender address
-    to: "test@naver.com",
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
+const mailOptions = {
+  from: 'runawayengineering@gmail.com',
+  to: 'destination@example.com',
+  subject: 'Email from runaway',
+  text: 'Test Email.'
+};
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+	console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
-
-main().catch(console.error);
