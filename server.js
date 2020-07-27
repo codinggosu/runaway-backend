c5890d4a830ceaba58126a8c5940ccf6d30a5090
cedricblaise <cblaise@bowdoin.edu>
2020-07-26 21:03:34 -0400

    Volunteer requests

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const bodyParser = require("body-parser");
 const Cors = require("cors");
-const http = require('http');
+const http = require("http");
 const express = require("express");
 const mongoose = require("mongoose");
 const auth = require("./route/auth/auth");
@@ -12,35 +12,43 @@ const server = http.createServer(app);
 
 // Swagger documentation
 const swaggerJSDoc = require("swagger-jsdoc");
-const swaggerUI = require("swagger-ui-express"); 
+const swaggerUI = require("swagger-ui-express");
 
 const options = {
-    definition: {
-        openapi: "3.0.0", // Specification (optional, defaults to swagger: '2.0')
-        info: {
-            title: "Runaway API", // Title (required)
-            version: "1.0.0", // Version (required)
-            servers: ["http://localhost:7000", "https://runaway-practicum.herokuapp.com"]
-        },
+  definition: {
+    openapi: "3.0.0", // Specification (optional, defaults to swagger: '2.0')
+    info: {
+      title: "Runaway API", // Title (required)
+      version: "1.0.0", // Version (required)
+      servers: [
+        "http://localhost:7000",
+        "https://runaway-practicum.herokuapp.com",
+      ],
     },
-        // Path to the API docs
-        apis: ["./route/*.js", "./schemas/schemas.js"]
-    };
+  },
+  // Path to the API docs
+  apis: ["./route/*.js", "./schemas/schemas.js"],
+};
 
 //Routes
 const blogRoute = require("./route/blog.js");
 const chatRoute = require("./route/chat.js");
 const eventRoute = require("./route/event.js");
 const resourceRoute = require("./route/resource.js");
-const surveyRoute = require("./route/survey.js")
+const surveyRoute = require("./route/survey.js");
+const volunteerRoute = require("./route/volunteer.js");
 
 //connect to the database // for now, the password will be in the file
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://runaway-database:2gn5YAq0BRwLc7tF@runaway-mrvci.mongodb.net/runaway?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://runaway-database:2gn5YAq0BRwLc7tF@runaway-mrvci.mongodb.net/runaway?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => {
-    console.log("Connection succeeded");
+  console.log("Connection succeeded");
 });
 
 // Initialize swagger-jsdoc -> returns validated swagger spec in json format
@@ -48,32 +56,28 @@ const swaggerSpec = swaggerJSDoc(options);
 
 //MiddleWare
 app.use(Cors());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
-
-
-app.get("/", function(req,res){
-    return res.status(200).send("Hello World");
-})
-
+app.get("/", function (req, res) {
+  return res.status(200).send("Hello World");
+});
 
 //Route function called
-blogRoute(app,mongoose);
-auth(app,mongoose);
-chatRoute(app,mongoose,server);
+blogRoute(app, mongoose);
+auth(app, mongoose);
+chatRoute(app, mongoose, server);
 eventRoute(app, mongoose);
 resourceRoute(app, mongoose);
 surveyRoute(app, mongoose);
+volunteerRoute(app, mongoose);
 
 app.set("json spaces", 2);
 
-
-
-server.listen(PORT,() => {
-    console.log("Hopefully the server is running on "+ PORT);
-})
+server.listen(PORT, () => {
+  console.log("Hopefully the server is running on " + PORT);
+});
 
 module.exports = app;
