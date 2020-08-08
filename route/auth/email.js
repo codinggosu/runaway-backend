0f739564b7a922cdd362cd2d6411a29476751c48
codinggosu <leedongjoo96@naver.com>
2020-08-08 17:01:32 +0900

    password reset link send implemented

--- route/auth/email.js
+++ route/auth/email.js
@@ -1,27 +1,26 @@
 require('dotenv').config()
-
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'runawayengineering@gmail.com',
-    pass: process.env.EMAIL_PASSWORD
-  }
-});
+module.exports = function(destination_email, subject, text){
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'runawayengineering@gmail.com',
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+  const mailOptions = {
+    from: 'runawayengineering@gmail.com',
+    to: destination_email,
+    subject: subject,
+    text: text
+  };
 
-const mailOptions = {
-  from: 'runawayengineering@gmail.com',
-  to: 'destination@example.com',
-  subject: 'Email from runaway',
-  text: 'Test Email.'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-	console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+    console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
--- route/auth/forgot_password.js
+++ route/auth/forgot_password.js
@@ -1,11 +1,39 @@
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
-//let User = require('../models/volunteerModel.js');
+const Crypto = require("crypto");
+const { userInfo } = require("os");
+const mail_sender = require('./email.js')
+
+let User = require('../../models/volunteerModel.js');
 
 module.exports = function (app, mongoose) {
     app.post("/api/volunteer/forgot-password/", function(req, res) { // ROUTING FOR forgot
     const email = req.body.email;
-    res.status(200).send(email);
+    User.findOne({email : email }, function(err, user) {
+        if (err || user==undefined) {
+            console.log("user not found");
+            res.status(400).send('user not found')
+        }
+        else {
+            Crypto.randomBytes(20, function(err, buf) {
+                let token = buf.toString("hex");
+                console.log(token, "is token")
+                reset_link = process.env.BASE_URL + '/reset' + token;
+                user.resetPasswordToken = token;
+                user.resetPasswordExpiration = Date.now() + 3600000;
+                user.save(function(err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        mail_sender(email, 'forgot your password?', reset_link);
+                        console.log("mail succcessfully sent");
+                        res.status(200).send('password reset link sent!')
+                    }
+                });
+            })
+        }
+    })
 //        passport.authenticate("login", { session: false }, function (err, user) {
 //            if (err) {
 //                console.log(err);
